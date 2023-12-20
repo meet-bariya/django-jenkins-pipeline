d1bd9281b4fc680335bcd7d7eb7c3c9671c59ad6
Meet Bariya <marutimeet@gmail.com>
2023-12-20 18:55:52 +0530

    venv script fixed

--- env-setup.sh
+++ env-setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [-d "venv"]
-then echo "Virtual Environment Already exists, Skipping this step"
+if [ -d "venv" ]
+then 
+    echo "Virtual Environment Already exists, Skipping this step"
 else
     python3 -m venv venv
 fi
 
-echo $PWD
+source ./venv/bin/activate
 
-source /venv/bin/activate
+pip3 install -r requirements.txt 
 
-pip install -r requirements.txt 
-
-if [-d "logs"]
-then echo "Logs directory Already exists, Skipping this step"
+if [ -d "logs" ]
+then 
+    echo "Logs directory Already exists, Skipping this step"
 else
     mkdir logs 
     touch logs/error.log logs/access.log
